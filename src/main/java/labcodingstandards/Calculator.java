c71e1aa971b9ba50624ee66bcdb484b33f114f81
jreyesr <jreyesr@users.noreply.github.com>
2020-06-18 11:48:21 -0500

    Edit style checker rules, fix errors

--- src/main/java/labcodingstandards/Calculator.java
+++ src/main/java/labcodingstandards/Calculator.java
@@ -1,5 +1,5 @@
-//
-//
+// Copyright (C) 2020
+// All rights reserved
 
 package labcodingstandards;
 
@@ -7,27 +7,30 @@ import java.util.Scanner;
 
 
 /**
- * @author
+ * @author José Reyes
  *
  */
 public class Calculator {	
-	
-	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
+	/**
+	 * Main entry point for Calculator class
+	 * @param args Not used
+	 */
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
 		
         System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
         
         char operator = reader.nextLine().charAt(0);
-        double First;
+        double first;
         double second;
         String input;
         
         while (true) {
-        	System.out.print("Enter first number: ");
-        	input = reader.nextLine();
+            System.out.print("Enter first number: ");
+            input = reader.nextLine();
         	
             try {
-            	First=Integer.parseInt(input);
+            	first=Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Not valid!");
@@ -35,8 +38,8 @@ public class Calculator {
         }
         
         while (true) {
-        	System.out.print("Enter second number: ");
-        	input = reader.nextLine();
+            System.out.print("Enter second number: ");
+            input = reader.nextLine();
         	
             try {
             	second=Integer.parseInt(input);
@@ -47,15 +50,23 @@ public class Calculator {
         }
 
         Calculator cal=new Calculator();
-        String result=cal.Operation(First,second,operator);
+        String result=cal.operation(first, second, operator);
 
         System.out.printf(result);
-		reader.close();
-	}
+        reader.close();
+    }
 
-	private String Operation(double first,double second,char operator) {
-		double result = 0;
-		switch(operator)
+    /**
+     * Compute a specified operation over two arguments
+     * @param first The first operand
+     * @param second The second operand
+     * @param operator The operation to compute. Must be one of '1', '2', '3', '4'
+     *                 for +, -, * and / respectively  
+     * @return A String with the form "The result is: _something_"
+     */
+    private String operation(double first, double second, char operator) {
+    	double result = 0;
+        switch(operator)
         {
             case '1':
                 result=first+second;
@@ -72,6 +83,6 @@ public class Calculator {
             default:
             	return "Error! operator is not correct";
         }
-		return "The result is: "+result;
-	}
+        return "The result is: "+result;
+    }
 }
